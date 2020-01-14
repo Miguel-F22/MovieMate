055b5b697c5a703dafcb7188dd1da323a514f8bf
Miguel F <miguel.a.figueroa.a@gmail.com>
2020-01-14 09:13:06 -0700

    Added Mark in ViewController

--- GroupProjectApp/GroupProjectApp/ViewController.swift
+++ GroupProjectApp/GroupProjectApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    MARK: View Did Load
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         /// Do any additional setup after loading the view.
