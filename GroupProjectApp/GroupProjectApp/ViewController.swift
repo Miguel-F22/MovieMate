5e79da1029d1099053c7464bba019d7b0ddc1c15
Miguel F <miguel.a.figueroa.a@gmail.com>
2020-01-14 08:30:17 -0700

    Just created the empty project for everyone to pull to their own machine

---
+++ GroupProjectApp/GroupProjectApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroupProjectApp
+//
+//  Created by Miguel Figueroa on 1/14/20.
+//  Copyright © 2020 Miguel Figueroa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
