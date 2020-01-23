cc848110aa48c6bae01ea2ea9979cb5e43ba93a8
Miguel F <miguel.a.figueroa.a@gmail.com>
2020-01-23 10:27:16 -0700

    Setup a files in the view folder for the OCEDetail, MovieDetail, MovieList views, and MovieTableViewCell. Also set up the main View controller so that it now displays the home page, has a title and the search bar now appears in the Nav Bar.

---
+++ GroupProjectApp/GroupProjectApp/View/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  GroupProjectApp
+//
+//  Created by Miguel Figueroa on 1/23/20.
+//  Copyright © 2020 Miguel Figueroa. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
