7b5694bcfbbefd85f7c75a429720faa2b9d349d1
Miguel F <miguel.a.figueroa.a@gmail.com>
2020-02-19 10:04:13 -0700

    Started the different approach, just adding a convenience init to the movie extension. Also added the dispatch main queue and reload collection to the movie detail file so that characters would populate when a new movie was chosen

--- GroupProjectApp/GroupProjectApp/Model/MovieExtension.swift
+++ GroupProjectApp/GroupProjectApp/Model/MovieExtension.swift
@@ -30,7 +30,25 @@ extension Movie {
     }
     
     
-    
+    convenience init(amovie: AMovie, context: NSManagedObjectContext = PersistenceService.context) {
+        var movie: Movie = Movie()
+        self.init(context: context)
+        
+        self.movieID = Int32(amovie.movieID)
+        self.title = amovie.title
+        self.overview = amovie.overview
+        self.posterPath = amovie.posterPath
+//        self.collection = amovie.collection
+        self.voteAverage = amovie.voteAverage
+        self.releaseDate = amovie.releaseDate
+//        self.language = amovie.language
+//        self.popularity = amovie.popularity
+        self.movieRelatedCharacters = NSSet(array: amovie.relatedCharacters ?? [] )
+        self.movieRelatedEvents = NSSet(array: amovie.relatedEvents ?? [] )
+        self.movieRelatedObjects = NSSet(array: amovie.relatedObjects ?? [] )
+        
+        
+    }
     
     static func createMovieRelations(movieToCreate: AMovie, notes: String, with context: NSManagedObjectContext) -> Movie? {
             let context = PersistenceService.context
