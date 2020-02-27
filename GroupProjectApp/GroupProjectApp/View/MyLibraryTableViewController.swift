455cbaca5494fe9be5287478fb5502bcae11f65f
Miguel F <miguel.a.figueroa.a@gmail.com>
2020-02-27 10:25:50 -0700

    fixed the my library nav title issue. started trying to get stuff to load in it

--- GroupProjectApp/GroupProjectApp/View/MyLibraryTableViewController.swift
+++ GroupProjectApp/GroupProjectApp/View/MyLibraryTableViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
 class MyLibraryTableViewController: UITableViewController, UISearchResultsUpdating {
     
     @IBOutlet weak var navTitle: UINavigationItem!
-    var CoreData: [Movie] = []
+    var coreData: [Movie]? = []
+    
     
     
 //    MARK: SEARCH BAR STUFF
@@ -23,7 +25,8 @@ class MyLibraryTableViewController: UITableViewController, UISearchResultsUpdati
     }
     
      func setupNavBar() {
-                
+                navigationController?.navigationBar.prefersLargeTitles = true
+        
             searchController = UISearchController(searchResultsController: nil)
             searchController?.searchResultsUpdater = self
             searchController?.obscuresBackgroundDuringPresentation = false
@@ -34,6 +37,10 @@ class MyLibraryTableViewController: UITableViewController, UISearchResultsUpdati
         }
 
 //    MARK: VIEW DID LOAD
+    
+    override func viewWillAppear(_ animated: Bool) {
+        coreData = fetchMovies()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,6 +53,20 @@ class MyLibraryTableViewController: UITableViewController, UISearchResultsUpdati
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    func fetchMovies() -> [Movie]? {
+        let context = PersistenceService.context
+
+        let fetchRequest = NSFetchRequest<Movie>(entityName: "Movie")
+        do {
+            let movies = try context.fetch(fetchRequest)
+            return movies
+           
+        } catch {
+            print(error)
+        }
+        return nil
+    }
 
     // MARK: - Table view data source
 
@@ -56,8 +77,26 @@ class MyLibraryTableViewController: UITableViewController, UISearchResultsUpdati
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return CoreData.count
+        return coreData?.count ?? 0
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "movieInfoIdentifier", for: indexPath) as? MovieTableViewCell else {
+            fatalError("No cell with id: movieInfoIdentifier that is a MovieTableViewCell 🤯")
+        }
+        guard let coreData = coreData else { return cell }
+
+        cell.movieTitle.text = coreData[indexPath.row].title
+        cell.movieDate.text = coreData[indexPath.row].releaseDate
+        cell.movieRating.text = String(coreData[indexPath.row].voteAverage)
+//        movieListImageController.fetchImage(path: CoreData[indexPath.row].posterPath) { image in
+//            DispatchQueue.main.async {
+//                cell.movieImage.image = image
+//
+//            }
+        
+        return cell
+}
     
 //    MARK: NAVIGATION
 
